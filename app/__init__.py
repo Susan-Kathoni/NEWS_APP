9f6787902c0e2f7e3bff9dc86f0f1684af7064ef
Susan-Kathoni <123joey4kathoniey@gmail.com>
2020-10-19 13:21:48 +0300

    Set up app configuration.

--- app/__init__.py
+++ app/__init__.py
@@ -1,7 +1,11 @@
 from flask import Flask
+from .config import DevConfig
 
 #Initializing application
 app = Flask(__name__)
+
+# Setting up configuration
+app.config.from_object(DevConfig)
 
 #Import views file in order to create views
 from app import views
