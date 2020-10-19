da48866b4e7877320436ffc924541ddb576fd6b9
Susan-Kathoni <123joey4kathoniey@gmail.com>
2020-10-19 13:24:11 +0300

    Add index function and the title for the template.

--- app/views.py
+++ app/views.py
@@ -3,12 +3,16 @@ from flask import render_template
 from app import app
 
 #Views
-#Convert news_id into an integer
+#Convert news_id into an integer.
 @app.route('/news/<int:news_id>')
-def news(news_id):
+def index():
 
     '''
     View news page function that returns news details page and its data
     '''
-    #Add message variable
-    return render_template('news.html',id = news_id)
+
+    title = 'Home - Welcome to The best News Website Online'
+    #Add message variable.
+    #Replace message variable with movie_id.
+    #Create a variable title and pass it to templates.
+    return render_template('news.html',title = title)
