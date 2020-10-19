8ae775c574b03dbceb14c4b7ca796308394a1d81
Susan-Kathoni <123joey4kathoniey@gmail.com>
2020-10-19 14:00:20 +0300

    Add instance _relative_config and append the contents of the config file to app.config.

--- app/__init__.py
+++ app/__init__.py
@@ -1,11 +1,14 @@
 from flask import Flask
 from .config import DevConfig
 
-#Initializing application
-app = Flask(__name__)
+# Initializing application.
+# Add instance_relative_config to connect to instance folder when creating app instance.
+app = Flask(__name__,instance_relative_config = True)
 
 # Setting up configuration
 app.config.from_object(DevConfig)
+# Connect to the config file and append its contents to app.config
+app.config.from_pyfile('config.py')
 
-#Import views file in order to create views
+# Import views file in order to create views
 from app import views
