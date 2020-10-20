40f2ab0b39527b97d777ae74ceeaa2a98e381ef2
Susan-Kathoni <123joey4kathoniey@gmail.com>
2020-10-20 13:09:38 +0300

    Create method that will test the behaviour of the News_article class.

---
+++ app/news_article_test.py
@@ -0,0 +1,25 @@
+import unittest
+from news_article_model import news_article
+News_article = news_article.News_article
+
+class NewsArticleTest(unittest.TestCase):
+    '''
+    Test Class to test the behaviour of the News_article class
+    '''
+# Method to instatiate News_article class to make self.new_news_article object.
+    def setUp(self):
+        '''
+        Set up method that will run before every Test
+        '''
+        self.new_news_article= News_article('https://nation.africa/kenya',' Caroline Wafula & Ruth Mbula', 'Live:Mashujaa Day celebrations in Kisii', 
+        'https://nation.africa/kenya/news/hundreds-line-up-to-join-mashujaa-day-celebrations-amid-tight-security-2485374', 'Hundreds line up to join Mashujaa Day celebrations amid tight security', 'https://nation.africa/resource/image/2485466/landscape_ratio16x9/960/540/5122a74e70d1c05a6aea741493cd7859/ee/gusii13.jpg', 'Tuesday, October 20, 2020', 'President Uhuru Kenyatta is expected to grace the important annual celebration later this morning, that follows months of preparations by Kisii County.' )
+
+    def test_instance(self):
+        self.assertTrue(isinstance(self.new_news_article,News_article))
+
+
+if __name__ == '__main__':
+    unittest.main()  
+
+                                                              
+                                                               
