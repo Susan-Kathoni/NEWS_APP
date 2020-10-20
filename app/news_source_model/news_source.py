2b3ec6e9d0d9dc5ca2d4852480c58a035b417546
Susan-Kathoni <123joey4kathoniey@gmail.com>
2020-10-20 13:08:26 +0300

    Create News_article class to define News_article objects.

---
+++ app/news_source_model/news_source.py
@@ -0,0 +1,10 @@
+class News_source:
+    '''
+    News_source class to define News_source objects
+    '''
+
+    def __init__(self,source, url, description, publishedAt):
+        self.source = source
+        self.author = author
+        self.url = url
+        
