94a3be4ef604abef15b1b83b79a800ed3e488d68
Susan-Kathoni <123joey4kathoniey@gmail.com>
2020-10-24 00:37:47 +0300

    Make changes.

--- app/main/views.py
+++ app/main/views.py
@@ -1,41 +1,32 @@
 from flask import render_template,request,redirect,url_for
 from . import main
-from ..requests import get_news_article,get_news_source,search_news_article
+from ..requests import get_news_sources,get_source_articles
 from .forms import ReviewForm
-from newsapi import NewsApiClient
-
-
-#Views
-#Convert news_id into an integer.
-# Create search() that passes in a dynamic variable.
+# from ..models import Review
 @main.route('/')
 def index():
-    # form = ReviewForm()
-    # news_article = get_news_article(url)
-
-    # if form.validate_on_submit():
-        title = form.title.data
-        review = form.review.data
-        new_review = Review(source, author, title,description, url,urlToImage, publishedAt, news_article.poster,review)
-        new_review.save_review()
-        return redirect(url_for('news_article',id = movie.id ))
-
     '''
-    View root page function that returns the index page and its data
+    View root page function that returns the index page and its data.
     '''
-
-    # Getting trending news_article
-    trending_news = get_news_article('trending')
-    developing_news = get_news_article('developing')
-    now_showing_news = get_news_article('now_playing')
     
-    title = 'Home - Best News Website online. '
+    business = get_news_sources('business')
+    technology = get_news_sources('technology')
+    sports = get_news_sources('sports')
+    entertainment = get_news_sources('entertainment')
     
-    search_news_article = request.args.get('news_article_query')
-    
-    if search_news_article:
-        return redirect(url_for('search', news_article_title=search_news_article))
+    title = 'Home -  Daily News'
+    search_news = request.args.get('news_query')
+    if search_news:
+        return redirect(url_for('search',news_name=search_news))
     else:
-        return render_template('index.html', title = title, trending = trending_news, developing = developing_news, now_playing = now_showing_news)
+        return render_template('index.html', title = title, business_sources = business, technology_sources = technology,sports_sources = sports,  entertainment_sources = entertainment)
 
-    return render_template('search.html', news_articles = searched_news_article)
+
+@main.route('/sources/<id>')
+def articles(id):
+    '''
+    view articles page
+    '''
+    articles = get_source_articles(id)
+    title = f'NH | {id}'
+    return render_template('news.html', title=title, articles=articles)
